be2a43e332fed6f34d1799e5127cdac4e78d869d
Володя <igor185097@gmail.com>
2022-10-27 22:04:04 +0300

    Removed comment the code

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,19 +1,6 @@
 import {expect, test} from "@playwright/test";
 import {faker} from "@faker-js/faker";
 
-//
-// const emailArray = ['123131ok@gmail.com', '123131ok@gmail.com', 'igor185097@gmail.com'];
-// const passwordArray = ['VitaB88', 'VitaB2228', 'VitaB2228']
-// for (const email of emailArray, passwordArray) {
-//     test(`testing with ${email}`, async ({page, email}) => {
-//         await page.goto('catalog?auth=login')
-//         await page.fill('#id_login_email', email)
-//         await page.fill('#id_login_password', 'VitaB88')
-//         await page.click('[type="submit"]')
-//         await expect(page.locator('.search-form__for'), 'search form not displayed after login').toBeVisible()
-//     });
-// }
-
 
 test('User can not login with uncorrect login', async ({page}) => {
     const email = faker.company
